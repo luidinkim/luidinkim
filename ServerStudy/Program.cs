611ece807a732e786a430e9f23f95935f15b1097
luidinkim <luidinkim@naver.com>
2021-01-31 22:19:07 +0900

    Initial commit

---
+++ ServerStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SeverCore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace SeverCore
+{
+    class Program
+    {
+        static void MainThread(object state)
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Hello Thread");
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(MainThread); //백그라운드에서 돌아간다. 일종의 StartCoroutine이네
+            //Thread t = new Thread(MainThread);
+            //t.Name = "Test Thread";
+            //t.IsBackground = true;
+            //t.Start();
+            //Console.WriteLine("Waiting For Thread");
+            //t.Join();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
